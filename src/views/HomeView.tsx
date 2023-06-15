27f1edfdfd01ea30fdbbcabb0ad3474f684f956d
Pat Nakajima <patnakajima@gmail.com>
2023-06-14 23:38:33 -0700

    Add copy to clipboard link

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,17 +1,28 @@
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement, useState } from "react";
 import ConversationListView from "./ConversationListView";
 import { useClient } from "../hooks/useClient";
-import { useConversation } from "../hooks/useConversation";
-import ConversationView from "./ConversationView";
 import { shortAddress } from "../util/shortAddress";
 
 export default function HomeView(): ReactElement {
   const client = useClient()!;
+  const [copied, setCopied] = useState(false);
+
+  function copy() {
+    navigator.clipboard.writeText(client.address);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  }
 
   return (
     <div className="p-4">
       <small>
-        Hi {shortAddress(client.address)}.<br />
+        Hi {shortAddress(client.address)}{" "}
+        <button className="text-xs text-zinc-600" onClick={copy}>
+          {copied ? "Copied!" : "Copy"}
+        </button>
+        <br />
         Here are your conversations:
       </small>
       <ConversationListView />
