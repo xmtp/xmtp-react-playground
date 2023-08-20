7afadff120b0eb0972c00153e154b52dbd3abe58
Lionel Lim <lionel.lim.l@gmail.com>
2023-08-20 23:47:01 +0800

    update login page

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,49 +1,21 @@
-import { ReactElement, useEffect } from "react";
-import Button from "../components/Button";
-import { useClient, useSetClient } from "../hooks/useClient";
-import { Wallet } from "ethers";
-import { Client } from "@xmtp/xmtp-js";
+import { ReactElement } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
-import {
-  AttachmentCodec,
-  RemoteAttachmentCodec,
-} from "@xmtp/content-type-remote-attachment";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function LoginView(): ReactElement {
-  const setClient = useSetClient();
-
-  async function generateWallet() {
-    const wallet = Wallet.createRandom();
-    const client = await Client.create(wallet, {
-      env: import.meta.env.VITE_XMTP_ENV,
-    });
-
-    // Don't do this in real life.
-    localStorage.setItem("_insecurePrivateKey", wallet.privateKey);
-
-    setClient(client);
-  }
 
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-12">
       <div className="mx-auto max-w-3xl"></div>
-      <div className="bg-white shadow sm:rounded-lg">
+      <div className="bg-white shadow sm:rounded-lg flex items-center justify-center">
         <div className="px-4 py-5 sm:p-6">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Login
+          <h3 className="text-base font-semibold leading-6 text-gray-900 text-center">
+            Scrib
           </h3>
           <div className="mt-2 max-w-xl text-sm text-gray-500">
-            <p>You can generate a wallet or connect your own.</p>
+            <p>Decentralised communicaiton. Start by connecting your wallet.</p>
           </div>
-          <div className="mt-5 flex space-x-4">
-            <Button
-              type="button"
-              onClick={generateWallet}
-              className="rounded-lg"
-            >
-              Generate Wallet
-            </Button>
+          <div className="mt-5 flex space-x-4 flex items-center justify-center">
             <div className="connect-button">
               <ConnectButton />
             </div>
